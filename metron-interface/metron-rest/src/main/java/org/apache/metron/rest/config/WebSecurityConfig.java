e4b7ead00e91aca9fbf1a002a0df379295893db1
justinjleet <justinjleet@gmail.com>
2018-10-18 10:49:03 -0400

    spring

--- metron-interface/metron-rest/src/main/java/org/apache/metron/rest/config/WebSecurityConfig.java
+++ metron-interface/metron-rest/src/main/java/org/apache/metron/rest/config/WebSecurityConfig.java
@@ -21,7 +21,10 @@ import static org.apache.metron.rest.MetronRestConstants.SECURITY_ROLE_ADMIN;
 import static org.apache.metron.rest.MetronRestConstants.SECURITY_ROLE_USER;
 
 import org.apache.metron.rest.MetronRestConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -31,6 +34,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
@@ -48,9 +53,31 @@ import java.util.List;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Controller
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(WebSecurityConfig.class);
 
     @Autowired
     private Environment environment;
+
+    @Value("${ldap.provider.url}")
+    private String providerUrl;
+    @Value("${ldap.provider.userdn}")
+    private String providerUserDn;
+    @Value("${ldap.provider.password}")
+    private String providerPassword;
+    @Value("${ldap.user.dn.patterns}")
+    private String userDnPatterns;
+    @Value("${ldap.user.passwordAttribute}")
+    private String passwordAttribute;
+    @Value("${ldap.user.searchBase}")
+    private String userSearchBase;
+    @Value("${ldap.user.searchFilter}")
+    private String userSearchFilter;
+    @Value("${ldap.group.searchBase}")
+    private String groupSearchBase;
+    @Value("${ldap.group.roleAttribute}")
+    private String groupRoleAttribute;
+    @Value("${ldap.group.searchFilter}")
+    private String groupSearchFilter;
 
     @RequestMapping(value = {"/login", "/logout", "/sensors", "/sensors*/**"}, method = RequestMethod.GET)
     public String handleNGRequests() {
@@ -92,6 +119,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureJdbc(AuthenticationManagerBuilder auth) throws Exception {
         List<String> activeProfiles = Arrays.asList(environment.getActiveProfiles());
+        if (activeProfiles.contains(MetronRestConstants.LDAP_PROFILE)) {
+            LOG.debug("Setting up LDAP authentication against %s", providerUrl);
+            if(this.providerUrl != null && !this.providerUrl.isEmpty()) {
+                auth.ldapAuthentication()
+                    .userDnPatterns(userDnPatterns)
+                    .userSearchBase(userSearchBase)
+                    .userSearchFilter(userSearchFilter)
+                    .groupRoleAttribute(groupRoleAttribute)
+                    .groupSearchFilter(groupSearchFilter)
+                    .groupSearchBase(groupSearchBase)
+                    .contextSource()
+                    .url(providerUrl)
+                    .managerDn(providerUserDn)
+                    .managerPassword(providerPassword)
+                    .and()
+                    .passwordCompare()
+                    .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
+                    .passwordAttribute(passwordAttribute);
+            }
+        }
         if (activeProfiles.contains(MetronRestConstants.DEV_PROFILE) ||
                 activeProfiles.contains(MetronRestConstants.TEST_PROFILE)) {
           auth.jdbcAuthentication().dataSource(dataSource)
@@ -102,10 +149,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         } else {
             auth.jdbcAuthentication().dataSource(dataSource);
         }
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
